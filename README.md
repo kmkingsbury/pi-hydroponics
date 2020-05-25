a8f9ab58de5b6473e4a2e2437d48b26b30a719ec
Kevin Kingsbury <kkingsbury@gmail.com>
2020-05-25 15:08:06 -0500

    Working config, DHT11 has issues with monit.

--- README.md
+++ README.md
@@ -6,3 +6,17 @@
     sudo apt-get install libgpiod2
 
 
+
+    pip install paho-mqtt
+
+
+    mosquitto_sub -h hostname -d -t home/hydropinics
+
+
+
+    pip install flake8 
+
+
+
+monit:
+https://spacemanlabs.io/2018/12/07/using-monit-to-monitor-and-restart-a-python-script/
--- climate_sensors.py
+++ climate_sensors.py
@@ -5,10 +5,13 @@ import logging
 import yaml
 import datetime
 
-# DS18B20.py
-# 2016-04-25
-# Public Domain
-
+import time
+import board
+import adafruit_dht
+ 
+# Initial the dht device, with data pin connected to:
+dhtDevice = adafruit_dht.DHT11(board.D17)
+ 
 # Typical reading
 # 73 01 4b 46 7f ff 0d 10 41 : crc=41 YES
 # 73 01 4b 46 7f ff 0d 10 41 t=23187
@@ -16,7 +19,29 @@ def c_to_f(temp_c = 0):
     temp_f = temp_c * 9.0 / 5.0 + 32.0
     return temp_f
 
+def writePidFile():
+    pid = str(os.getpid())
+    currentFile = open('/tmp/climate_sensors.pid', 'w')
+    currentFile.write(pid)
+    currentFile.close()
 
+
+#logging.basicConfig(level=logging.INFO, format=' %(asctime)s - %(levelname)s - %(message)s')
+logger = logging.getLogger('sprinker')
+logger.setLevel(logging.INFO)
+
+fh = logging.FileHandler('./climate_sensors.log')
+fh.setLevel(logging.INFO)
+
+ch = logging.StreamHandler()
+ch.setLevel(logging.INFO)
+
+formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+fh.setFormatter(formatter)
+ch.setFormatter(formatter)
+
+logger.addHandler(fh)
+logger.addHandler(ch)
 
 with open("config.yml", 'r') as ymlfile:
     cfg = yaml.load(ymlfile, Loader=yaml.FullLoader)
@@ -31,6 +56,8 @@ if cfg['mqtt']['enabled'] == True:
    # Loop start: These functions implement a threaded interface to the network loop. Calling loop_start() once, before or after connect*(), runs a thread in the background to call loop() automatically. This frees up the main thread for other work that may be blocking.
    client.loop_start()
 
+
+writePidFile()
 while True:
 
    for sensor in glob.glob("/sys/bus/w1/devices/28-*/w1_slave"):
@@ -43,16 +70,37 @@ while True:
          if "YES" in data:
             (discard, sep, reading) = data.partition(' t=')
             t = c_to_f(float(reading) / 1000.0)
-            print("{} {:.1f}".format(id, t))
+            logger.info("{} {:.1f}".format(id, t))
             s = datetime.datetime.utcnow().strftime('%Y-%m-%d %H:%M:%S.%f')
             timestamp =  s[:-3]
 
             if cfg['mqtt']['enabled'] == True:
                 client.publish(cfg['mqtt']['topic'],  '{ "temperature_' + str(id) + '":"' + str(t) + '", "datetime":"' + str(timestamp) + '" }') # publish to mqtt
          else:
-            print("999.9")
+            logger.info("Temperature sensor error")
 
       except:
          pass
+
+
+   try:
+     # Print the values to the serial port
+     temperature_c = dhtDevice.temperature
+     temperature_f = temperature_c * (9 / 5) + 32
+     humidity = dhtDevice.humidity
+     logger.info(
+        "Temp: {:.1f} F / {:.1f} C    Humidity: {}% ".format(
+             temperature_f, temperature_c, humidity
+          )
+      )
+     if cfg['mqtt']['enabled'] == True:
+          client.publish(cfg['mqtt']['topic'],  '{ "temperature_dht' +  '":"' + str(temperature_f) + '", "datetime":"' + str(timestamp) + '" }') # publish to mqtt
+          client.publish(cfg['mqtt']['topic'],  '{ "humidity' +  '":"' + str(humidity) + '", "datetime":"' + str(timestamp) + '" }') # publish to mqtt
+   except RuntimeError as error:
+     # Errors happen fairly often, DHT's are hard to read, just keep going
+     logger.info(error.args[0])
+
+
+
 
    time.sleep(1.0)
